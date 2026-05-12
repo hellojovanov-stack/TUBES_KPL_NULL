0ce071c0a21229edd4a8d54f91c14b9fd6962e25
Steven Taufik Fajar <zenvenpro@gmail.com>
2026-05-12 08:43:38 +0700

    refactor(steven): change DbC to safer Defensive Programming in Response

--- backend/helpers/response.php
+++ backend/helpers/response.php
@@ -5,19 +5,29 @@ class Response {
     public static function json(
         $success,
         $message,
-        $data = []
+        $data = [],
+        $statusCode = 200
     ) {
-
+        // ==========================================
+        // IMPLEMENTASI DEFENSIVE PROGRAMMING
+        // ==========================================
+        if (!is_int($statusCode)) {
+            $statusCode = (int) $statusCode;
+            if ($statusCode < 100 || $statusCode > 599) {
+                $statusCode = 200;
+            }
+        }
+        if (!is_string($message)) {
+            $message = (string) $message;
+        }
+        // ==========================================
+        http_response_code($statusCode);
         header('Content-Type: application/json');
-
         echo json_encode([
-
             "success" => $success,
             "message" => $message,
             "data"    => $data
-
         ]);
-
         exit;
     }
 }
